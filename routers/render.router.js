bcae9abd475761f0a2ff911142090e64cced1e63
Nava Slutzkin <nava0533160319@gmail.com>
2026-09-06 19:54:22 +0300

    Add user render retrieval functionality and update routing

--- routers/render.router.js
+++ routers/render.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createRender } = require('../controllers/render.controller.js');
+const { createRender, getUserRenders, getRenderById } = require('../controllers/render.controller.js');
 const requireAuth = require('../middlewares/auth.middleware.js'); // ייבוא המידלוור מהקובץ שלך
 
 // הנתיב מקבל קודם כל את דרישת ההתחברות (requireAuth), ורק אם היא עוברת, הוא ממשיך לפונקציית היצירה
 router.post('/', requireAuth, createRender);
+router.get('/', requireAuth, getUserRenders);
+router.get('/:id', requireAuth, getRenderById);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express'); // ייבוא ספריית Express
 const cors = require('cors'); // ייבוא ספריית CORS שהיא מאפשרת לקבל בקשות לשרת ממקורות שונים
 const mongoose = require('mongoose'); //חיבור לדטא-בייס
 const authRoutes = require('./routers/auth.router.js'); //מייבאת את הנתונים מהקובץ הזה
+const renderRoutes = require('./routers/render.router.js'); // ייבוא נתיבי ההדמיות.
 
 const app = express(); // יצירת מופע של אפליקציית Express
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);// במקרה והכתובת מתחילה במה שכתוב פה, השרת ילך לקובץ המוגדר
+app.use('/api/renders', renderRoutes); // חיבור נתיבי ההדמיות לכתובת הבסיסית.
 
 const PORT = process.env.PORT || 5000; // הגדרת הכתובת שעליה ירוץ האתר והוספת ערך ברירת מחדל
 
